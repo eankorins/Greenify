ee1adcb3126d8dfbaa8f60f0551bebe458631cc5
Clythez <Clythez@gmail.com>
2014-04-21 19:29:44 +0200

    Maps
    Maps added:
    
    Easy 1
    Hard 1

--- Assets/Scripts/MapRunner.cs
+++ Assets/Scripts/MapRunner.cs
@@ -67,7 +67,7 @@ public class MapRunner : MonoBehaviour
                 {
                     int depth = (int)(OpenTiles.Count / filledCount) * depthCount + 1;
                     bool check = OpenTiles.Where(i => i.tag != "Taken").Select(i => i.GetComponent<Tile>().checkIfPath(depth, false)).Any(i=> i == false);
-                    if (temp.hasPath() && !check)
+                    if (temp.hasPath() && !check  && sweepForWallRight())
                     {
                         Path curMove = new Path();
                         curMove = temp.nextMove();
